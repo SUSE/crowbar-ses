bb3514d94b5f07809049086a8708458cfc59a15e
Tim Serong <tserong@suse.com>
2016-03-23 23:33:23 +1100

    Resync pacemaker_service_object from crowbar-ha
    Signed-off-by: Tim Serong <tserong@suse.com>

--- crowbar_framework/app/models/pacemaker_service_object.rb
+++ crowbar_framework/app/models/pacemaker_service_object.rb
@@ -23,20 +23,11 @@
 #
 
 class PacemakerServiceObject < ServiceObject
-
   #
   # Eigenclass with methods used by ServiceObject
   #
 
   class << self
-
-    # This is the key that allows to find out that an element item is a
-    # pacemaker cluster: if the element name is $cluster_key:$foo, then it's
-    # one. Otherwise, it's not.
-    def cluster_key
-      "cluster"
-    end
-
     # Note that we cannot cache the list of clusters here as we're in a
     # eigenclass, and so the cache will be longer term than a single request
     # (hence we won't notice new clusters). It's therefore up to the callers to
@@ -52,26 +43,37 @@ class PacemakerServiceObject < ServiceObject
       clusters
     end
 
-    # Returns: name of the barclamp and of the proposal for this cluster
-    def cluster_get_barclamp_and_proposal(element)
-      if is_cluster?(element)
-        ["pacemaker", cluster_name(element)]
-      else
-        [nil, nil]
+    # Returns: List of available clusters including remotes
+    def available_remotes
+      remotes = {}
+      # we only care about the deployed clusters, not about existing
+      # proposals
+      RoleObject.find_roles_by_name("pacemaker-config-*").select do |role|
+        elements = role.override_attributes["pacemaker"]["elements"]
+        elements["pacemaker-remote"] && !elements["pacemaker-remote"].empty?
+      end.each do |role|
+        remotes["#{remotes_key}:#{role.inst}"] = role
       end
+      remotes
+    end
+
+    # Returns: the element name for the cluster that is the match of the
+    # remotes element
+    def cluster_from_remotes(element)
+      return nil unless is_remotes?(element)
+
+      "#{cluster_key}:#{cluster_name(element)}"
+    end
+
+    # This is the key that allows to find out that an element item is a
+    # pacemaker cluster excluding remote nodes: if the element name is
+    # $cluster_key:$foo, then it's one. Otherwise, it's not.
+    def cluster_key
+      "cluster"
     end
 
     def is_cluster?(element)
       element.start_with? "#{cluster_key}:"
-    end
-
-    # Returns: name of the cluster, or nil if it's not a cluster
-    def cluster_name(element)
-      if is_cluster? element
-        element.gsub("#{cluster_key}:", "")
-      else
-        nil
-      end
     end
 
     def cluster_vhostname_from_name(name)
@@ -83,26 +85,79 @@ class PacemakerServiceObject < ServiceObject
       "cluster-#{name.gsub("_", "-")}.#{ChefObject.cloud_domain}"
     end
 
-    def cluster_vhostname_from_element(element)
-      if is_cluster? element
-        cluster_vhostname_from_name(cluster_name(element))
+    # This is the key that allows to find out that an element item is a
+    # pacemaker cluster including remote nodes: if the element name is
+    # $remotes_key:$foo, then it's one. Otherwise, it's not.
+    def remotes_key
+      "remotes"
+    end
+
+    def is_remotes?(element)
+      element.start_with? "#{remotes_key}:"
+    end
+
+    def remotes_remote_nodes_count(element)
+      return 0 unless is_remotes?(element)
+
+      role = RoleObject.find_role_by_name("pacemaker-config-#{cluster_name(element)}")
+      elements = role.override_attributes["pacemaker"]["elements"]
+      elements["pacemaker-remote"].nil? ? 0 : elements["pacemaker-remote"].length
+    end
+
+    # Returns: name of the barclamp and of the proposal for this cluster
+    def cluster_get_barclamp_and_proposal(element)
+      return [nil, nil] unless is_cluster?(element) || is_remotes?(element)
+
+      ["pacemaker", cluster_name(element)]
+    end
+
+    # Returns: name of the cluster, or nil if it's not a cluster
+    def cluster_name(element)
+      case
+      when is_remotes?(element)
+        element.gsub("#{remotes_key}:", "")
+      when is_cluster?(element)
+        element.gsub("#{cluster_key}:", "")
       else
         nil
       end
+    end
+
+    def cluster_vhostname_from_element(element)
+      return nil unless is_cluster?(element)
+
+      cluster_vhostname_from_name(cluster_name(element))
+    end
+
+    # Get the founder of a cluster, based on the element
+    def cluster_founder(element)
+      return nil unless is_cluster?(element)
+
+      cluster = cluster_name(element)
+      NodeObject.find("pacemaker_founder:true AND pacemaker_config_environment:pacemaker-config-#{cluster}").first
     end
 
     # Returns: list of nodes in the cluster, or nil if the cluster doesn't exist
     def expand_nodes(cluster)
-      clusters = available_clusters
-      if clusters[cluster].nil?
-        nil
-      else
-        pacemaker_proposal = clusters[cluster]
-        cluster_nodes = pacemaker_proposal.override_attributes["pacemaker"]["elements"]["pacemaker-cluster-member"]
-        cluster_nodes || []
-      end
+      pacemaker_proposal = available_clusters[cluster]
+      return nil if pacemaker_proposal.nil?
+
+      cluster_nodes = pacemaker_proposal.override_attributes["pacemaker"]["elements"]["pacemaker-cluster-member"]
+      cluster_nodes || []
     end
 
+    # Returns: list of remote nodes in the cluster, or nil if the cluster doesn't exist
+    def expand_remote_nodes(cluster)
+      pacemaker_proposal = available_remotes[cluster]
+      return nil if pacemaker_proposal.nil?
+
+      remote_nodes = pacemaker_proposal.override_attributes["pacemaker"]["elements"]["pacemaker-remote"]
+      remote_nodes || []
+    end
+  end
+
+  def expand_remote_nodes(cluster)
+    PacemakerServiceObject.expand_remote_nodes(cluster)
   end
 
   #
@@ -119,7 +174,7 @@ class PacemakerServiceObject < ServiceObject
   def role_expand_elements(role, role_name)
     elements = role.override_attributes[@bc_name]["elements"][role_name]
     expanded_nodes = nil
-    if role.override_attributes[@bc_name].has_key?("elements_expanded")
+    if role.override_attributes[@bc_name].key?("elements_expanded")
       expanded_nodes = role.override_attributes[@bc_name]["elements_expanded"][role_name]
     end
     elements ||= []
@@ -134,6 +189,57 @@ class PacemakerServiceObject < ServiceObject
     end
 
     [elements, all_nodes, has_expanded]
+  end
+
+  # !!! Horrible workaround until we fix crowbar orchestration !!!
+  # Because each chef-client runs executes everything in all cookbooks, and
+  # because the crm calls slow things down considerably, we have a drift issue
+  # where the founder (which does much more work since it's the only one doing
+  # the crm calls) goes much slower than the other nodes. This can impact
+  # things badly when it's so slow that it triggers the timeout in the sync
+  # marks.
+  # The workaround here is to reset the sync marks on the founder, so that all
+  # nodes wait on the sync marks and the drift is only happening between sync
+  # marks.
+  # The goal is to only do this when applying a proposal, so that other
+  # chef-client runs are not blocked waiting for sync marks.
+  def self.reset_sync_marks_on_cluster_founder(founder, cluster)
+    return if founder.nil? ||
+        founder[:pacemaker].nil? ||
+        founder[:pacemaker][:sync_marks].nil? ||
+        founder[:pacemaker][:sync_marks][cluster].nil?
+
+    founder[:pacemaker][:sync_marks][cluster].keys.each do |sync_mark|
+      # The pacemaker_setup sync mark (see the crowbar-pacemaker::default
+      # recipe) requires special handling: it is created by the founder in
+      # Chef's convergence phase, but waited for by all non-founders in Chef's
+      # compile phase, because they need it in order to copy the authkey
+      # attribute from the founder to themselves and then invoke the
+      # corosync::authkey_writer recipe.
+      #
+      # This is only required for initial pacemaker setup and running in
+      # compile phase, so we don't want to reset it.  If we were to reset it,
+      # then every time a proposal was applied, the non-founder nodes would be
+      # blocked in their compile phase until the founder reached the point in
+      # its convergence phase where it creates the pacemaker_setup sync mark,
+      # and this would be too long for the non-founders to wait when the run
+      # list is long.
+      next if sync_mark == "pacemaker_setup"
+      founder[:pacemaker][:sync_marks][cluster].delete(sync_mark)
+    end
+
+    founder.save
+  end
+
+  def reset_sync_marks_on_clusters_founders(elements)
+    elements.each do |element|
+      next unless PacemakerServiceObject.is_cluster? element
+
+      founder = PacemakerServiceObject.cluster_founder(element)
+      cluster = cluster_name(element)
+
+      PacemakerServiceObject.reset_sync_marks_on_cluster_founder(founder, cluster)
+    end
   end
 
   # This allocates a virtual IP for the cluster in each network in networks
@@ -175,7 +281,7 @@ class PacemakerServiceObject < ServiceObject
     new_allocation = false
 
     elements.each do |element|
-      if PacemakerServiceObject.is_cluster? element
+      if PacemakerServiceObject.is_cluster?(element)
         ok, new = allocate_virtual_ips_for_cluster_in_networks(element, networks)
         new_allocation ||= new
       end
@@ -198,7 +304,7 @@ class PacemakerServiceObject < ServiceObject
     # cookbook code is executed faster. So far, it seems it's not an issue
     # (we're not even hitting the "will run chef-client a second time if first
     # one fails), but this would still need to be improved.
-    system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path)
+    system("sudo", "-i", Rails.root.join("..", "bin", "single_chef_client.sh").expand_path.to_s)
   end
 
   # This prepares attributes so that, if ha_enabled is true, the chef run will
@@ -211,7 +317,7 @@ class PacemakerServiceObject < ServiceObject
     dirty = false
 
     elements.each do |element|
-      next unless PacemakerServiceObject.is_cluster? element
+      next unless PacemakerServiceObject.is_cluster?(element)
 
       cluster = cluster_name(element)
 
@@ -249,7 +355,7 @@ class PacemakerServiceObject < ServiceObject
 
     data = role.default_attributes
     attribute_path[0, attribute_path.length - 1].each { |attribute|
-      if not (data.has_key?(attribute) && data[attribute].is_a?(Hash))
+      if not (data.key?(attribute) && data[attribute].is_a?(Hash))
         data[attribute] = {}
       end
       data = data[attribute]
